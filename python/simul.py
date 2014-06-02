846b5aaafd6ac5ca9e7f5a90963bc473ccc018fd
cphyc <admin@cphyc.me>
2014-06-01 23:54:40 -0700

    Work with copy of the array ...

--- python/simul.py
+++ python/simul.py
@@ -1,10 +1,36 @@
 #!/usr/env python3
-import numpy as np
+try:
+    import numpypy as np
+except:
+    import numpy as np
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 import ipdb
+
+## Tri Diagonal Matrix Algorithm(a.k.a Thomas algorithm) solver
+def TDMAsolver(a, b, c, d):
+    '''
+    TDMA solver, a b c d can be NumPy array type or Python list type.
+    refer to http://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
+    '''
+    nf = len(a) # number of equations
+    ac, bc, cc, dc = map(np.array, (a, b, c, d)) # copy the array
+    for it in xrange(1, nf):
+        mc = ac[it]/bc[it-1]
+        bc[it] = bc[it] - mc*cc[it-1]
+        dc[it] = dc[it] - mc*dc[it-1]
+ 
+    xc = ac
+    xc[-1] = dc[-1]/bc[-1]
+ 
+    for il in xrange(nf-2, -1, -1):
+        xc[il] = (dc[il]-cc[il]*xc[il+1])/bc[il]
+ 
+    del bc, cc, dc # delete variables from memory
+ 
+    return xc
 
 class Vector(object):
     def __init__(self, parent):
@@ -22,7 +48,13 @@ class Vector(object):
         self.compute_G()
 
         # new += dt/2*(3G-G_old)
-        self.field = self.field + self.p.dt/2*(3*self.G - self.G_old) 
+        self.field[1:-1] = (self.field[1:-1] 
+                            + self.p.dt/2*(3*self.G[1:-1] - self.G_old[1:-1])
+                        ) 
+
+        # conditions at top and bottom : null
+        self.field[0 ,:] = 0
+        self.field[-1,:] = 0
 
     def compute_G(self):
         raise Exception("Vector class is a base class, not supposed to be "+
@@ -31,12 +63,11 @@ class Vector(object):
     def initial(self, init_cond):
         if init_cond == 'null':
             self.field = np.zeros((self.p.Nz, self.p.NFourier))
-        else: 
-            try:
-                self.field = np.array([[init_cond(n,k) for n in range(self.p.NFourier)]
-                                       for k in range(self.p.Nz)])
-            except TypeError:
-                raise Exception("init_cond must be either `null` or a 2-arguments callable")
+        elif init_cond == "T":
+            self.field = np.array([[T_0(n,k,self.p) for n in range(self.p.NFourier)]
+                                   for k in range(self.p.Nz)])
+        else:
+            raise Exception("init_cond must be either `null` or `T`")
 
 class Temp(Vector):
     name = "T"
@@ -72,18 +103,17 @@ class Vort(Vector):
                 self.wk1[k,n] = 1 /(dia(k)-sub_f(k)*self.wk2[k-1,n])
                 self.wk2[k,n] = sup(k)*self.wk1[k,n]
 
-            self.wk1[-1,n] = 1/(dia(self.p.Nz-1)-sub_f(self.p.Nz-1)*self.wk2[n,-2])
+            self.wk1[-1,n] = 1/(dia(self.p.Nz-1)-sub_f(self.p.Nz-1)*self.wk2[-2,n])
 
             self.sub[:,n] = [sub_f(k) for k in range(self.p.Nz)]
 
-        
-    def compute_G(self):
+    def step(self):
         rhs = self.p.psi.field
         # boundary conditions k=0, Nz-1 : psi = 0
         rhs[0, :] = 0
         rhs[-1,:] = 0
-        for n in range(1,self.p.NFourier):  
 
+        for n in range(1,self.p.NFourier):  
             # tridiag. solver
             self.field[0,n] = rhs[0,n]*self.wk1[0,n]
             for k in range(1, self.p.Nz):
@@ -107,7 +137,7 @@ class Simulation(object):
     param_list = {'Re': 1, 'Pr': 1, 'Ra': 1, 'a' : 1, 'Nz': 100,
                   'NFourier': 50, 'dt_security': 0.9,
                   'maxiter': 100, 'freq_output': 10,
-                  'freq_critical_Ra':50}
+                  'freq_critical_Ra':50, 'verbose': False}
 
     def __init__(self, *args, **kargs):
         # save the default parameters
@@ -156,22 +186,23 @@ class Simulation(object):
                         for n in range(self.NFourier) ])
 
         # save the arrays for next output
-        self.T_old = self.T.field[self.Nz//3,:]
-        self.omega_old = self.omega.field[self.Nz//3,:]
-        self.psi_old = self.psi.field[self.Nz//3,:]
+        ipdb.set_trace()
+        self.T_old = self.T.field[self.Nz//3,:].copy()
+        self.omega_old = self.omega.field[self.Nz//3,:].copy()
+        self.psi_old = self.psi.field[self.Nz//3,:].copy()
 
         return out+"\n"
 
     def step(self):
         # eventually output
-        if self.niter % self.freq_output == 0:
+        if self.verbose and self.niter % self.freq_output == 0:
             self.dump()
 
         # eventually calculate the d-ln term for the critical Ra
-        if self.niter % self.freq_critical_Ra == 0 :
+        if self.verbose and self.niter % self.freq_critical_Ra == 0 :
             output = "# growth : \t"
             output+= "".join([
-                "{T.name}_{n}\t{w.name}_{n}\t{psi.name}_{n}".format(T=self.T,
+                "{T.name}_{n}\t{w.name}_{n}\t{psi.name}_{n}\t".format(T=self.T,
                                                                     w=self.omega,
                                                                     psi=self.psi,
                                                                     n=n)
@@ -179,6 +210,7 @@ class Simulation(object):
             output+= "\n"
             output+= "# growth : \t"
             output+= self.growth()
+
             print(output)
 
         # get the max timestep
@@ -239,6 +271,4 @@ if __name__ == '__main__':
         s.psi.step()
         s.omega.step()
 
-    #del s
-        
-        
+    del s
